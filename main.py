95f9a3bf0e450cb3da854c1e58779a22c5bf004c
Paulo Moggi <paulohpmoggi@gmail.com>
2016-05-30 23:28:26 -0300

    Modifica tamanho da tela e botões da janela principal

--- main.py
+++ main.py
@@ -6,9 +6,6 @@ import cv2, cv,PIL
 
 imHandler = ImageHandler(100)
 
-def onCamera():
-    # Entrando em modo live da camera
-    print('Camera!')
 
 def onCapture(frame, currentTime):
 
@@ -21,9 +18,6 @@ def onCapture(frame, currentTime):
     cv.SaveImage(name, frame)
     Qimg = OpenCVQImage(frame)
 
-    print('Capture!')
-
-
     bytes=Qimg.bits().asstring(Qimg.numBytes())
     img = Image.frombytes("RGB",(Qimg.width(),Qimg.height()),bytes)
 
@@ -31,7 +25,7 @@ def onCapture(frame, currentTime):
 
     imHandler.remove_background(img,[200,200,200], processedName)
 
-    for i, background in enumerate(os.listdir(backgroundsLocation)):    
+    for i, background in enumerate(os.listdir(backgroundsLocation)):
         background = 'images/backgrounds/' + background
         #background = PIL.Image.open(str(backgroundPath))
         #imHandler.put_background(background, processedName)
@@ -45,10 +39,10 @@ def onCapture(frame, currentTime):
 def main():
 
     app = QtGui.QApplication(sys.argv)
+
     window = WindowWidget()
-    window.camera.connect(onCamera)
     window.capture.connect(onCapture)
-    
+
     sys.exit(app.exec_())
 
 if __name__ == '__main__':
--- source/window.py
+++ source/window.py
@@ -13,44 +13,38 @@ from source.camera import *
 class WindowWidget(QtGui.QMainWindow):
 
 
-    camera = QtCore.pyqtSignal()
     capture = QtCore.pyqtSignal(cv.iplimage, str)
     background = QtCore.pyqtSignal();
     backgroundPath = ''
 
     def __init__(self, parent=None):
         super(QtGui.QMainWindow, self).__init__(parent)
-        self.setMinimumSize(800,640)
+        self.setMinimumSize(320,240)
         self.setWindowTitle('Awesome Window')
-
         self.init_ui()
 
     def init_ui(self):
-        
+
         cameraDevice = CameraDevice(mirrored=True,parent=self)
         self.cameraWidget = CameraWidget(cameraDevice,self)
         self.cameraWidget.show()
 
-        #############
+        # Ajusta a janela para o tamanho da imagem da camera
+        self.setGeometry(0, 0,self.cameraWidget.width(), self.cameraWidget.height())
+
         menuContainer = QtGui.QWidget(self)
-        menuContainer.setMinimumSize(400,40)
+        menuContainer.setMinimumSize(320,40)
         menuLayout = QtGui.QHBoxLayout()
 
-        cameraButton = QtGui.QPushButton("CAMERA")
-        cameraButton.setMinimumSize(100,40)
-        cameraButton.clicked.connect(self._clickedCamera)
-        menuLayout.addWidget(cameraButton)
+        #############
 
         captureButton = QtGui.QPushButton("CAPTURE")
-        captureButton.setMinimumSize(100,40)
+        captureButton.setMinimumSize(50,40)
         captureButton.clicked.connect(self._clickedCapture)
+
+        spacer = QtGui.QSpacerItem(20, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
         menuLayout.addWidget(captureButton)
-
-        # backgroundSelectButton = QtGui.QPushButton("BACKGROUND")
-        # backgroundSelectButton.setMinimumSize(100,40)
-        # backgroundSelectButton.clicked.connect(self._clickedBackgroundSelec)
-        # menuLayout.addWidget(backgroundSelectButton)
-
+        menuLayout.addSpacerItem(spacer)
         menuContainer.setLayout(menuLayout)
         menuContainer.show()
 
@@ -59,16 +53,12 @@ class WindowWidget(QtGui.QMainWindow):
         self.show()
 
     def _clickedCapture(self):
-        #self.currentTime = str(datetime.datetime.now())
         self.currentTime = str(time.ctime())
         self.capture.emit(self.cameraWidget._frame, self.currentTime)
         self.test = FinalPhotoSelect(self.currentTime)
-        
-    def _clickedCamera(self):
-        self.camera.emit()
 
     # def _clickedBackgroundSelec(self):
-    #     fname = QtGui.QFileDialog.getOpenFileName(self, 'Open file', 
+    #     fname = QtGui.QFileDialog.getOpenFileName(self, 'Open file',
     #             os.getcwd() + '/images/backgrounds')
     #     fname = fname[str(fname).find("images/backgrounds/"):]
     #     self.backgroundPath = str(fname)
@@ -76,14 +66,14 @@ class WindowWidget(QtGui.QMainWindow):
 
 
 class imgWindowWidget(QtGui.QWidget):
-    
+
     def __init__(self, path):
         super(QtGui.QWidget, self).__init__()
         self.path_to_email = path
         self.initUI(path)
-        
+
     def initUI(self,path):
-        
+
         okButton = QtGui.QPushButton("Aceitar")
         okButton.clicked.connect(self._showDialog)
         cancelButton = QtGui.QPushButton("Cancelar")
@@ -96,28 +86,26 @@ class imgWindowWidget(QtGui.QWidget):
         vbox = QtGui.QVBoxLayout()
         vbox.addStretch(1)
         vbox.addLayout(hbox)
-        
-        self.setLayout(vbox)   
+
+        self.setLayout(vbox)
         ibox = QtGui.QHBoxLayout(self)
         imageMap = QtGui.QPixmap(path)
-        imageMap = imageMap.scaled(640, 480, QtCore.Qt.KeepAspectRatio) 
+        imageMap = imageMap.scaled(640, 480, QtCore.Qt.KeepAspectRatio)
         label = QtGui.QLabel(self)
         label.setPixmap(imageMap)
         ibox.addWidget(label)
         label.setGeometry(0,0,640,480)
-        
-        
+
         self.setGeometry(0, 0, 800, 640)
-        self.setWindowTitle('Imagem Resultante')  
+        self.setWindowTitle('Imagem Resultante')
 
         self.show()
-        
-       
+
 
     def _showDialog(self):
-        text, ok = QtGui.QInputDialog.getText(self, 'Email Input', 
+        text, ok = QtGui.QInputDialog.getText(self, 'Email Input',
             'Insira seu email')
-        
+
         if ok:
 
             emailsender = EmailSender('smtp.gmail.com', 587)
@@ -151,7 +139,7 @@ class FinalPhotoSelect(QtGui.QWidget):
         self.scene.addItem(self.image)
         self.view.centerOn(self.image)
         self._images = []
-        
+
         for file in os.listdir(os.getcwd() + '/images/outputs'):
             if self.currentTime in file:
                 if 'transparent' not in file:
@@ -178,10 +166,3 @@ class FinalPhotoSelect(QtGui.QWidget):
             self.image.setPixmap(self._images[val])
         except IndexError:
             print "Error: No image at index", val
-
-# if __name__ == "__main__":
-#     app = QtGui.QApplication([])
-#     w = FinalPhotoSelect()
-#     w.show()
-#     w.raise_()
-#     app.exec_()
